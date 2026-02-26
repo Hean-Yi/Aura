84679ae2fcf44b9c6b15b98b5d291e36a9881bf1
1so-H3an <lhc2005523@163.com>
2026-02-26 13:28:42 +0800

    Initial Commit

---
+++ Aura/AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraApp.swift
+//  Aura
+//
+//  Created by 梁航川 on 2026/2/26.
+//
+
+import SwiftUI
+
+@main
+struct AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aura/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aura
+//
+//  Created by 梁航川 on 2026/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
